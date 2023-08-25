cab6c2a1bdaa684bf83a8bde02a38c0e9b9e9f82
WinterJackson <winterjacksonwj@gmail.com>
2023-08-25 14:37:59 +0300

    Completed Assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,44 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, name in enumerate(dwarves, start=1):
+        print(f"{index}. {name}")
 
-def summon_captain_planet():
-    pass
+dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+roll_call_dwarves(dwarf_names)
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteer_calls):
+    capitalized_calls = [call.capitalize() + "!" for call in planeteer_calls]
+    return capitalized_calls
 
-def find_the_cheese():
-    pass
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+captain_planet_calls = summon_captain_planet(planeteer_calls)
+print(captain_planet_calls)
+
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
+
+# Example usage
+short_words = ["puff", "go", "two"]
+print(long_planeteer_calls(short_words))  # Output: False
+
+assorted_words = ["two", "go", "industrious", "bop"]
+print(long_planeteer_calls(assorted_words)) 
+
+def find_the_cheese(ingredients):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for ingredient in ingredients:
+        if ingredient in cheese_types:
+            return ingredient
+    return None
+
+snacks = ["crackers", "gouda", "thyme"]
+print(find_the_cheese(snacks))  
+
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+print(find_the_cheese(soup))  
+
+ingredients = ["garlic", "rosemary", "bread"]
+print(find_the_cheese(ingredients))  
+
